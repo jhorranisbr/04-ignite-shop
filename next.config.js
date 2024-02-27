edc20e47367e94efaa57ae39d51c41cb56fbfb13
Jhorranis <jhorranis@gmail.com>
2024-02-27 16:36:07 -0300

    first commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  images: {
+    remotePatterns: [
+      {
+        hostname: 'files.stripe.com'
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
